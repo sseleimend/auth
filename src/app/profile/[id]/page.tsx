be23db0ebd53d7c8b5c1b204d45614ac2458c00c
Victor Seleimend <sseleimend@gmail.com>
2025-06-22 15:35:45 -0300

    feat: add Profile component to display user ID

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default function Profile({ params }: { params: { id: string } }) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>
+        User{" "}
+        <span className="p-2 ml-2 rounded bg-orange-500 text-black">
+          {params.id}
+        </span>
+      </h1>
+    </div>
+  );
+}
