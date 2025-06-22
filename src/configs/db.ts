fa958911d1efca5db2db6c8b6e056a50bc37c9d0
Victor Seleimend <sseleimend@gmail.com>
2025-06-22 15:08:30 -0300

    feat: implement MongoDB connection setup

---
+++ src/configs/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB connected succesfully");
+    });
+    connection.on("error", (err) => {
+      console.log("MongoDB connection error. " + err);
+      process.exit(1);
+    });
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    else console.error(error);
+  }
+}
