40be2caed7baa282180161914d07fae6a11c3922
Victor Seleimend <sseleimend@gmail.com>
2025-06-22 15:35:25 -0300

    feat: enhance Sign Up page with user input fields and layout

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
 export default function SignUpPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
   return (
-    <div className="flex">
-      <h1 className="text-center text-white text-2xl">Sign Up</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Sign Up</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name="username"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="Username"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+      <button
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Sign Up
+      </button>
+      <Link href="/login">Login here</Link>
     </div>
   );
 }
