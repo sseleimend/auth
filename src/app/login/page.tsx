7c8f7e90f8183f4b4a53a7f1dc169d733f17c038
Victor Seleimend <sseleimend@gmail.com>
2025-06-22 15:35:33 -0300

    feat: implement Login page with user input fields and layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = () => {};
+
   return (
-    <div className="flex">
-      <h1 className="text-center text-white text-2xl">Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Login
+      </button>
+      <Link href="/signup">Signup here</Link>
     </div>
   );
 }
