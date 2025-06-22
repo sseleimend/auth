2a583ddfb9767cd5f302e3e6ebcac24355178fba
Victor Seleimend <sseleimend@gmail.com>
2025-06-22 20:09:12 -0300

    feat: implement login functionality with form validation and error handling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,51 @@
 "use client";
 
+import axios from "axios";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  const onLogin = () => {};
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setButtonDisabled(true);
+
+      const response = await axios.post("/api/users/login", user);
+
+      toast.success("Login success");
+
+      router.push("/profile");
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("An unexpected error occurred.");
+      }
+    } finally {
+      setButtonDisabled(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Toaster />
       <h1>Login</h1>
       <hr />
       <label htmlFor="email">Email</label>
@@ -36,6 +69,7 @@ export default function LoginPage() {
         placeholder="Password"
       />
       <button
+        disabled={buttonDisabled}
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
