9357f9fecc73e24468099021dd661c516e4e37eb
Victor Seleimend <sseleimend@gmail.com>
2025-06-22 20:08:50 -0300

    feat: implement user signup endpoint with validation and password hashing

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,51 @@
+import { connect } from "@/configs/db";
+import User from "@/models/User.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const { username, email, password } = await request.json();
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+
+    console.log(savedUser);
+
+    return NextResponse.json(
+      {
+        message: "User created successfully",
+        savedUser,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error instanceof Error ? error.message : error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
