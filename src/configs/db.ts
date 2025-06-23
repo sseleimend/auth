5bd1c39bfa084ebd2a6330f1a7da31946da443f0
Victor Seleimend <sseleimend@gmail.com>
2025-06-23 11:28:55 -0300

    feat: configure mongoose connection with database name from environment variables

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
+    mongoose.connect(process.env.MONGO_URI!, {
+      dbName: process.env.MONGO_DBNAME!,
+    });
     const connection = mongoose.connection;
     connection.on("connected", () => {
       console.log("MongoDB connected succesfully");
