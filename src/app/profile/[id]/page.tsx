95fbe4189f5e854e96f42b5dce166bef19d8a2cd
Victor Seleimend <sseleimend@gmail.com>
2025-06-23 12:22:57 -0300

    feat: simplify Profile component by removing ProfileParams interface and directly using Params type

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,20 +1,13 @@
-interface ProfileParams {
-  id: string;
-}
+type Params = Promise<{ id: string }>;
 
-interface ProfileProps {
-  params: ProfileParams;
-}
+export default async function Profile({ params }: { params: Params }) {
+  const { id } = await params;
 
-export default function Profile(props: ProfileProps) {
-  const { params } = props;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>
         User{" "}
-        <span className="p-2 ml-2 rounded bg-orange-500 text-black">
-          {params.id}
-        </span>
+        <span className="p-2 ml-2 rounded bg-orange-500 text-black">{id}</span>
       </h1>
     </div>
   );
