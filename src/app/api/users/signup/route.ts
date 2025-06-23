20220aa03454933e77a4ca74e51f25f29fbd014a
Victor Seleimend <sseleimend@gmail.com>
2025-06-23 11:29:20 -0300

    feat: send verification email upon user signup

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/configs/db";
 import User from "@/models/User.model";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { sendMail } from "@/helpers/mailer";
 
 connect();
 
@@ -29,7 +30,7 @@ export async function POST(request: NextRequest) {
     });
     const savedUser = await newUser.save();
 
-    console.log(savedUser);
+    await sendMail({ email, emailType: "VERIFY", userId: savedUser._id });
 
     return NextResponse.json(
       {
